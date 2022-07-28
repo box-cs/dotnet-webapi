da8b565684a23fc5b22bfcd7b256dd66c0ad1036
anactualfox <foxablessc2@gmail.com>
2022-07-27 19:56:58 -0600

    added reaction time dto

---
+++ Users/Dtos/ReactionTimeGameSessionDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Users.Dtos
+{
+    public class ReactionTimeGameSessionDto
+    {
+        [Required]
+        public string UserId { get; init; }
+        [MinLength(1)]
+        [Required]
+        public List<long> ReactionTimes { get; init; }
+    }
+}
